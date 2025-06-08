eb8fb0b713f791d0ff1ae5a6cae5b0bf0e47bd14
nospecdev <nospecdev@gmail.com>
2025-06-08 18:25:54 -0500

    Added speech synthesis functionality. When a dog is detected, it will now tell you 'Dog detected' and when a dog is no longer detected, it will tell you 'No dog detected'

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# dog-detector
+# Dog Detector
+It does exactly what it sounds like! It detects dogs.
+
+**Link to project:** http://recruiters-love-seeing-live-demos.com/
+
+![alt tag](http://placecorgi.com/1200/650)
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript, TensorFlowJS, NodeJS, Express
+
+I linked a pre-trained model from TensorFlowJS called Coco-SSD to my index, so it will be loaded when the view is requested from the server. It requests permission to use your device's camera, then begins checking every frame for... Dogs.
+
+I used the Model-View-Controller framework to keep everything nice and tidy.
+
+## Optimizations
+
+At first, I was repeating myself a lot when handling the change of style and inner text of the header(ie, "DOG DETECTED!" or "NO DOG DETECTED."). I decided it would make more sense to put those in their own functions and call them when needed. Encapsulation, baby.
+
+Currently, there's an issue that comes up when a dog is detected for one frame, then isn't the next. It causes the utterances to loop. This can be solved by making sure a dog was or was not detected for x amount of frames before it decides to speak. That will help make sure it's not causing many instances of talkDog and stopDog to be queued.
+
+## Lessons Learned:
+
+I learned that it's important to keep track of the scope of your functions. I got stuck for a while trying to get the stopDog function to trigger whenever no dog was detected or when there were no objects detected at all. It was because I was trying to implement it inside of my loop.
--- public/js/script.js
+++ public/js/script.js
@@ -15,29 +15,56 @@ const cam = async () => {
 const main = async () => {
   await cam()
   const model = await cocoSsd.load()
-  
+  let checkDogState = false
+
   const detectFrame = async () => {
     const guesses = await model.detect(video);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(guesses.length);
+    let dogDetected = guesses.some(detects => detects.class === 'dog');
+
+
+
     guesses.forEach((detects) => {
-      if(detects.class === 'dog'){
+      if(dogDetected){
+        if(detects.class === 'dog'){
         const [x, y, w, h] = detects.bbox;
         ctx.strokeStyle = 'blue';
         ctx.lineWidth = 2;
         ctx.strokeRect(x, y, w, h);
         ctx.fillStyle = 'blue';
         ctx.fillText(`${detects.class} ${Math.round(detects.score * 100)}%`, x, y - 5)
-        dogDetected = true
-        const utterance = new SpeechSynthesisUtterance("Dog detected!");
-        speechSynthesis.speak(utterance);
-      } else {
-        dogDetected = false
+          if(dogDetected && !checkDogState){
+            talkDog()
+          }
+          checkDogState = true;
+          console.log(dogDetected)
+          console.log(checkDogState)
+          console.log(typeof detects)
+        }
       }
     });
+    if (guesses.length === 0 && checkDogState) {
+      stopDog()
+      checkDogState = false
+    }
     requestAnimationFrame(detectFrame)
   }
-
   detectFrame()
+}
+
+const talkDog = () => {
+  document.querySelector('.dog').innerText = 'DOG DETECTED!';
+  document.querySelector('.dog').style.backgroundColor = 'lime';
+  const utterance = new SpeechSynthesisUtterance("Dog detected!");
+  speechSynthesis.speak(utterance);
+}
+
+const stopDog = () => {
+  document.querySelector('.dog').innerText = 'NO DOG DETECTED!';
+  document.querySelector('.dog').style.backgroundColor = 'red';
+  const utterance = new SpeechSynthesisUtterance("No dog detected!");
+  speechSynthesis.speak(utterance);
 }
 
 main()
